540ee67c6afd428fd4d3d9d5fd1da2cfa21392c2
Audrius Pasvenskas <apasvenskas@outlook.com>
2023-10-31 12:10:49 -0700

    auth and user routers setup

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,6 +1,6 @@
 // Require `checkUsernameFree`, `checkUsernameExists` and `checkPasswordLength`
 // middleware functions from `auth-middleware.js`. You will need them here!
-
+const router = require('express').Router()
 
 /**
   1 [POST] /api/auth/register { "username": "sue", "password": "1234" }
@@ -25,6 +25,9 @@
   }
  */
 
+  router.post('/register', (req, res, next) => {
+    res.json('register')
+  })
 
 /**
   2 [POST] /api/auth/login { "username": "sue", "password": "1234" }
@@ -41,6 +44,10 @@
     "message": "Invalid credentials"
   }
  */
+
+  router.post('/login', (req, res, next) => {
+    res.json('login')
+  })
 
 
 /**
@@ -59,5 +66,11 @@
   }
  */
 
+  router.get('/logout', (req, res, next) => {
+    res.json('logout')
+  })
+
  
 // Don't forget to add the router to the `exports` object so it can be required in other modules
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
-
+const userRoutes = require('./users/users-router')
+const authRoutes = require('./auth/auth-router')
 /**
   Do what needs to be done to support sessions with the `express-session` package!
   To respect users' privacy, do NOT send them a cookie unless they log in.
@@ -24,6 +25,9 @@ server.use(cors());
 server.get("/", (req, res) => {
   res.json({ api: "up" });
 });
+
+server.use('/api/users', userRoutes)
+server.use('/api/auth', authRoutes)
 
 server.use((err, req, res, next) => { // eslint-disable-line
   res.status(err.status || 500).json({
